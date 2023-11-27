b8a8a9bd8408dc63dd69bbe56b6ca798afa476b5
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-11-27 00:43:54 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,23 @@ const loginRoute = require("./routes/login");
 
 const port = process.env.PORT || 5000;
 
+// Configure CORS for specific origins
+const allowedOrigins = ['https://mern-omega-livid.vercel.app/', 'http://localhost:3000'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST'], // Add other allowed methods as needed
+    allowedHeaders: ['Content-Type', 'Authorization'], // Add required headers
+    credentials: true,
+    preflightContinue: true, // Enable handling preflight requests
+};
+
 // Connect to the MongoDB using the connection function
 connectToMongo()
     .then((db) => {
