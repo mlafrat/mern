748095d7512b375528a1a042e67fbe30ba6d2204
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-11-26 23:43:01 -0500

    test

---
+++ server/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config({ path: "./.env" });
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connectToMongo = require("./db/conn");
+
+const loginRoute = require("./routes/login");
+
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to the MongoDB using the connection function
+connectToMongo()
+    .then((db) => {
+        console.log("Connected to MongoDB");
+
+        // Use the database object (db) in your routes or wherever needed
+        app.use("/login", loginRoute(db));
+
+        app.listen(port, () => {
+            console.log(`Server is running on port: ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
